7ec8f9e3b740799fd111234bd9b01b4e5389ebb7
vatiza <rakibur.mac@gmail.com>
2023-12-21 20:13:15 +0600

    sum

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
-document.getElementById("calculate-btn").addEventListener("click",function(){
-    
+document.getElementById("calculate-btn").addEventListener("click", function () {
+    const netIncome = document.getElementById("income").value;
+    const incomeStringToFloat = parseFloat(netIncome);
+
+    const getFoodField = document.getElementById("food-field").value;
+    const footStringToFloat = parseFloat(getFoodField);
+
+    const getRentField = document.getElementById("rent-field").value;
+    const rentStringToFloat = parseFloat(getRentField);
+
+    const getClothesField = document.getElementById("clothes-field").value;
+    const clothesToFloat = parseFloat(getClothesField);
+
+    const totalsExpensesSum = footStringToFloat + rentStringToFloat + clothesToFloat;
+    console.log(totalsExpensesSum);
+
+
+
 })
