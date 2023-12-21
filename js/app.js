cd429ed31d4fb138501cf176d2d516dde68814d7
vatiza <rakibur.mac@gmail.com>
2023-12-21 21:43:38 +0600

    done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
+
+
+
 document.getElementById("calculate-btn").addEventListener("click", function () {
     const netIncome = document.getElementById("income").value;
-    const incomeStringToFloat = parseFloat(netIncome);
-
+const incomeStringToFloat = parseFloat(netIncome);
     const getFoodField = document.getElementById("food-field").value;
     const footStringToFloat = parseFloat(getFoodField);
 
@@ -15,12 +17,33 @@ document.getElementById("calculate-btn").addEventListener("click", function () {
     console.log(totalsExpensesSum);
 
     const setTotalExpenses = document.getElementById("expenses-show");
-   setTotalExpenses.innerText=totalsExpensesSum;
+    setTotalExpenses.innerText = totalsExpensesSum;
 
-   const calculateRemingBalance=incomeStringToFloat-totalsExpensesSum;
-   const setremaingBalance=document.getElementById("balance");
-   setremaingBalance.innerText=calculateRemingBalance;
+    const calculateRemingBalance = incomeStringToFloat - totalsExpensesSum;
+    const setremaingBalance = document.getElementById("balance");
+    setremaingBalance.innerText = calculateRemingBalance;
 
-  
+})
+
+
+document.getElementById("percentancebtn").addEventListener("click", function () {
+    const netIncome = document.getElementById("income").value;
+    const incomeStringToFloat = parseFloat(netIncome);
+
+    const getpercentanceValue = document.getElementById("numberofpercentance").value;
+    const percentanceValueFloat = parseFloat(getpercentanceValue);
+
+    const percentanceCalculation = ((incomeStringToFloat * percentanceValueFloat) / 100);
+
+    const setSavingAmount = document.getElementById("savingAmount");
+    setSavingAmount.innerText = percentanceCalculation;
+
+ const getBalance=document.getElementById("balance").innerText;
+ const balanceToFloat=parseFloat(getBalance);
+ console.log(balanceToFloat)
+ const calculatationRemainingBalance=balanceToFloat-percentanceCalculation;
+ const setReminingBalance=document.getElementById("afterbalance");
+ setReminingBalance.innerText=calculatationRemainingBalance;
+
 
 })
